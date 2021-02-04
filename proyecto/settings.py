02c10a818c9ab58dd7e4b6b834fbff25c6e19177
debsdaniel <debs.foros@gmail.com>
2021-02-03 21:10:30 -0600

    01-006 Configurar Base de Datos

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orm_dj2',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5435',
     }
 }
 
