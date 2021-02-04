1f59210efba86c5ddae75e9ffbb1e133b9b22306
debsdaniel <debs.foros@gmail.com>
2021-02-03 21:43:35 -0600

    01-009 Primer Modelo

---
+++ app1/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    descripcion = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.descripcion
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria,self).save()
+
+    class Meta:
+        verbose_name_plural= "Categorias"
