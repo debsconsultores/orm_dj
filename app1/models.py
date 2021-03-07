0deb003a701d29d6b1d1aa738a05bbaca5ceaf26
debsdaniel <debs.foros@gmail.com>
2021-03-07 00:38:59 -0600

    02-017 Usar el campo slug con django para una mayor legibilidad

--- app1/models.py
+++ app1/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import DO_NOTHING
 
+from django.utils.text import slugify
 
 class ModeloAuditoria(models.Model):
     fecha_crea = models.DateTimeField(auto_now_add=True)
@@ -160,9 +161,14 @@ class Hijo(models.Model):
 
 class Publicacion(models.Model):
     titulo = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=60,default="")
 
     def __str__(self):
         return self.titulo
+
+    def save(self):
+        self.slug = slugify(self.titulo)
+        super(Publicacion,self).save()
 
 
 class Articulo(models.Model):
